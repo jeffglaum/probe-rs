0de411175bf4ef4a628a794254461d9c30a17deb
Jeff Glaum <jeffgla@microsoft.com>
2024-06-11 12:44:51 -0700

    Established initial Infineon PSOC6 files.

--- probe-rs/src/vendor/infineon/mod.rs
+++ probe-rs/src/vendor/infineon/mod.rs
@@ -4,7 +4,13 @@ use probe_rs_target::Chip;
 
 use crate::{
     config::DebugSequence,
-    vendor::{infineon::sequences::xmc4000::XMC4000, Vendor},
+    vendor::{
+        infineon::sequences::{
+            xmc4000::XMC4000,
+            psoc6::PsoC62Series,
+        }, 
+        Vendor,
+    },
 };
 
 pub mod sequences;
@@ -17,7 +23,10 @@ impl Vendor for Infineon {
     fn try_create_debug_sequence(&self, chip: &Chip) -> Option<DebugSequence> {
         let sequence = if chip.name.starts_with("XMC4") {
             DebugSequence::Arm(XMC4000::create())
-        } else {
+        } else if chip.name.starts_with("PSoC_62") {
+            DebugSequence::Arm(PsoC62Series::create())
+        }
+            else {
             return None;
         };
 
--- probe-rs/src/vendor/infineon/sequences/mod.rs
+++ probe-rs/src/vendor/infineon/sequences/mod.rs
@@ -1,3 +1,4 @@
 //! Infineon debug sequences.
 
 pub mod xmc4000;
+pub mod psoc6;
